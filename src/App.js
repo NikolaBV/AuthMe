ba55aceebe4287fc7e018625ce409b9e130e65e7
fmi <fmi@fmi.uni-plovdiv.bg>
2024-04-15 10:37:12 +0300

    Bug fix: typo on the onClick functions

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
   };
   return (
     <>
-      <button onClick={handleLogoutPopup} class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+      <button onClick={handleLoginPopup} class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
       Log in
 </button>
 <button onClick={handleLogoutPopup} class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
